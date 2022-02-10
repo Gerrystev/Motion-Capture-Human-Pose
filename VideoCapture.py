586e2b1751092b70f1d2456f9410f5cda4adea66
Gerry Steven <sgerrys93@gmail.com>
2022-02-10 14:21:34 +0700

    Multi Processing

--- VideoCapture.py
+++ VideoCapture.py
@@ -1,12 +1,14 @@
 import multiprocessing
+import cv2
 import time
 
-class OutputFeed:
-    def __init__(self, v_queue, o_queue):
+class VideoCapture:
+    def __init__(self, queue):
         # store the video stream object and output path, then initialize
         # the most recently read frame, thread for reading frames, and
         # the thread stop event
         self.frame = None
+        
         self.fps = multiprocessing.Queue()
         
         # used to record the time when processed last frame
@@ -14,12 +16,17 @@ class OutputFeed:
         # used to record the time at which processed current frame
         self.new_frame_time = 0
         
-        self.current_frame = None
+        # rtsp://<username>:<password>@<ip_address>:<port>/Streaming/Channels/<id>
+        # initialize widget properties
+        self.cap = None
+        self.currentFrame = None
         
-        self.process = multiprocessing.Process(target=self.video_loop)
+        self.process = multiprocessing.Process(target=self.video_capture)
         
-        self.v_queue = v_queue
-        self.o_queue = o_queue        
+        # queue for processing image
+        self.queue = queue
+        
+        self.cap_queue = multiprocessing.Queue()
         
     def start(self):
         self.process.start()
@@ -35,17 +42,19 @@ class OutputFeed:
         self.fps.put(int(1/(self.new_frame_time - self.prev_frame_time)))
         self.prev_frame_time = self.new_frame_time
         
-    def process_frame(self):
-        time.sleep(.1)
-        self.o_queue.put(self.current_frame)
         
-    def video_loop(self):
-        # if waitingFrame is not empty render current object
+    def video_capture(self):
+        # video capture loop
+        self.cap = cv2.VideoCapture("rtsp://192.168.1.2:8080/h264_ulaw.sdp")
+        # self.cap = cv2.VideoCapture(0)
+        self.cap_queue.put(self.cap)
+        
         while True:
-            if not self.v_queue.empty():
-                self.current_frame = self.v_queue.get()
-                    
-                self.process_frame()
+            _, frame = self.cap.read()
+            if frame is not None:
+                frame = cv2.flip(frame, 1)
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+                self.queue.put(frame)
                 self.calculate_fps()
         
     def destroy_window(self):
--- gui.py
+++ gui.py
@@ -2,61 +2,96 @@ import tkinter as tk
 import cv2
 import PIL.Image
 import PIL.ImageTk
+import multiprocessing
 from tkinter import *
 from tkinter import ttk
 
-from VideoFeed import VideoFeed
+from VideoCapture import VideoCapture
 from OutputFeed import OutputFeed
 
-root = Tk()
-frame = ttk.Frame(root, padding=20)
+def update_video(fps_queue, image_label, fps_label, queue):
+   width, height = 640, 360 
+   if not queue.empty(): 
+       fps = fps_queue.get()
+       frame = queue.get()
+       frame = cv2.flip(frame, 1)
+       frame = cv2.resize(frame, (width, height))
+       img = PIL.Image.fromarray(frame)
+       imgtk = PIL.ImageTk.PhotoImage(image=img)
+       image_label.configure(image=imgtk)
+       image_label._image_cache = imgtk  # avoid garbage collection
+   
+       # put fps into label
+       fps_label.configure(text=str(fps) + " FPS")
+       root.update()
 
-# Labels
-title_display = Label(text="Human Pose Estimation", font=("Tahoma", 24))
-video_label_display = Label(text="Video Feed", font=("Tahoma", 20))
-output_label_display = Label(text="3D Estimation", font=("Tahoma", 20))
-video_fps_display = Label(text="0 FPS", font=("Tahoma", 12))
-output_fps_display = Label(text="0 FPS", font=("Tahoma", 12))
+def update_all(root, v_label, o_label, v_fps, o_fps, v_fps_label, 
+               o_fps_label, v_queue, o_queue):
+   update_video(v_fps, v_label, v_fps_label, v_queue)
+   update_video(o_fps, o_label, o_fps_label, o_queue)
+   
+   root.after(0, func=lambda: update_all(root, v_label, o_label, v_fps, o_fps, v_fps_label, 
+               o_fps_label, v_queue, o_queue))
 
-# Capture video
-output_feed = OutputFeed(root, output_fps_display)
-video_feed = VideoFeed(root, video_fps_display, output_feed)
+if __name__ == '__main__':
+    root = Tk()
+    frame = ttk.Frame(root, padding=20)
+    
+    # labels
+    video_display = Label(root)
+    output_display = Label(root)
+    title_display = Label(text="Human Pose Estimation", font=("Tahoma", 24))
+    video_label_display = Label(text="Video Feed", font=("Tahoma", 20))
+    output_label_display = Label(text="3D Estimation", font=("Tahoma", 20))
+    video_fps_display = Label(text="0 FPS", font=("Tahoma", 12))
+    output_fps_display = Label(text="0 FPS", font=("Tahoma", 12))    
+    
+    # check button
+    save_fbx = False
+    check_button_display = ttk.Checkbutton(root, text='Save to .fbx', variable=save_fbx,
+     	    onvalue=True, offvalue=False)
+    
+    # start record button
+    button_display = ttk.Button(root, text="Start Record", width=200)
+    
+    # display everything to grid
+    title_display.grid(row=0, column=0, columnspan=2, pady=10, stick="nsew")
+    video_label_display.grid(row=1, column=0, padx=10, sticky="nsew")
+    output_label_display.grid(row=1, column=1, padx=10, sticky="nsew")
+    video_display.grid(row=2, column=0, padx=10, sticky="nsew")
+    output_display.grid(row=2, column=1, padx=10, sticky="nsew")
+    video_fps_display.grid(row=3, column=0, pady=20, sticky="nsew")
+    output_fps_display.grid(row=3, column=1, pady=20, sticky="nsew")
+    check_button_display.grid(row=4, column=0, columnspan=2, pady=10)
+    button_display.grid(row=5, column=0, columnspan=2, pady=10)
+    
+    # # Grid for full screen
+    # Grid.rowconfigure(root, 0, weight=1)
+    # Grid.rowconfigure(root, 1, weight=1)
+    # Grid.rowconfigure(root, 2, weight=1)
+    # Grid.rowconfigure(root, 3, weight=1)
+    # Grid.rowconfigure(root, 4, weight=1)
+    # Grid.columnconfigure(root, 0, weight=1)
+    # Grid.columnconfigure(root, 1, weight=1)
 
-video_display = video_feed.label
-output_display = output_feed.label
-
-# Check button
-save_fbx = False
-check_button_display = ttk.Checkbutton(root, text='Save to .fbx', variable=save_fbx,
- 	    onvalue=True, offvalue=False)
-
-# Start record button
-button_display = ttk.Button(root, text="Start Record", width=200)
-
-# Display everything to grid
-title_display.grid(row=0, column=0, columnspan=2, pady=10, stick="nsew")
-video_label_display.grid(row=1, column=0, padx=10, sticky="nsew")
-output_label_display.grid(row=1, column=1, padx=10, sticky="nsew")
-video_display.grid(row=2, column=0, padx=10, sticky="nsew")
-output_display.grid(row=2, column=1, padx=10, sticky="nsew")
-video_fps_display.grid(row=3, column=0, pady=20, sticky="nsew")
-output_fps_display.grid(row=3, column=1, pady=20, sticky="nsew")
-check_button_display.grid(row=4, column=0, columnspan=2, pady=10)
-button_display.grid(row=5, column=0, columnspan=2, pady=10)
-
-video_feed.videoLoop()
-
-# # Grid for full screen
-# Grid.rowconfigure(root, 0, weight=1)
-# Grid.rowconfigure(root, 1, weight=1)
-# Grid.rowconfigure(root, 2, weight=1)
-# Grid.rowconfigure(root, 3, weight=1)
-# Grid.rowconfigure(root, 4, weight=1)
-# Grid.columnconfigure(root, 0, weight=1)
-# Grid.columnconfigure(root, 1, weight=1)
-
-root.mainloop()
-video_feed.destroy_window()
-output_feed.destroy_window()
-cv2.destroyAllWindows()
+    # initalize multiprocessing
+    v_queue = multiprocessing.Queue()
+    o_queue = multiprocessing.Queue()
+    
+    video_capture = VideoCapture(v_queue)
+    output_feed = OutputFeed(v_queue, o_queue)
+    
+    video_capture.start()
+    output_feed.start()
+    
+    update_all(root, video_label_display, output_label_display, 
+               video_capture.fps, output_feed.fps, 
+               video_fps_display, output_fps_display, v_queue, o_queue)
+    
+    
+    
+    root.mainloop()
+    video_capture.destroy_window()
+    output_feed.destroy_window()
+    cv2.destroyAllWindows()
 
